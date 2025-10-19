e431c5bc5a7969a792293fe657645262d5b21db7
deepalakshmivyoma-beep <deepalakshmi.vyoma@gmail.com>
2025-10-19 15:44:05 +0530

    Displaying the new json in UI

--- src/data/questions/index.ts
+++ src/data/questions/index.ts
@@ -2,12 +2,14 @@
 import ramayana from './ramayana.json'
 import mahabharata from './mahabharata.json'
 import sanskritGrammar from './sanskrit-grammar.json'
+import ThePuzzleofWorkJoyEvolutionV1 from './The-Puzzle-of-Work-Joy-Evolution-V1.json'
 
 // Map of all available quizzes
 export const quizzes = {
   'ramayana': ramayana,
   'mahabharata': mahabharata,
   'sanskrit-grammar': sanskritGrammar,
+  'The-Puzzle-of-Work-Joy-Evolution-V1':ThePuzzleofWorkJoyEvolutionV1
 }
 
 // Get list of all quiz theme names
